189b6103c73cc45f066e3dfc6c61e642e15f97a4
skiperr254 <skiperr254@gmail.com>
2024-01-15 01:32:55 +0300

    Restructured the database schema.

--- base/models.py
+++ base/models.py
@@ -4,8 +4,10 @@ from django.db import models
 
 class RoomMember(models.Model):
     name = models.CharField(max_length=200)
+    kihi
     uid = models.CharField(max_length=1000)
     room_name = models.CharField(max_length=200)
+
     insession = models.BooleanField(default=True)
 
     def __str__(self):
